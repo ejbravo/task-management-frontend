8ba1a31997fa7d937cda7e5e0467d06be4044785
ejbravo <ejbravo@users.noreply.github.com>
2021-11-08 10:52:03 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.eb16d5a3e7e236a64b51a260bf5c10e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2436238524be5455bb5ed05782b0e62",
+    "url": "https://ejbravo.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "bda20343c7ad0a9a08bf",
+    "url": "https://ejbravo.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "bd19c0a4d0839a9b7cdb",
+    "url": "https://ejbravo.github.io/task-management-frontend/static/js/2.b0f1a2c6.chunk.js"
+  },
+  {
+    "revision": "bda20343c7ad0a9a08bf",
+    "url": "https://ejbravo.github.io/task-management-frontend/static/js/main.c2c96211.chunk.js"
+  },
+  {
+    "revision": "3101a5b3cb1dc90ba6e5",
+    "url": "https://ejbravo.github.io/task-management-frontend/static/js/runtime~main.4edaca72.js"
+  }
+]);
